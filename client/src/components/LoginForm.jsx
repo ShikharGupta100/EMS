b15a12b379823e71a9361b2686ab5dc75637f120
Your Name <mr.shikhar100@gmail.com>
2026-05-27 23:46:25 +0530

    Button Added Day-3

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import LoginLeftSide from "./LoginLeftSide"
-import { ArrowBigLeftIcon, EyeIcon, EyeOffIcon } from "lucide-react"
+import { ArrowBigLeftIcon, EyeIcon, EyeOffIcon, Loader2Icon } from "lucide-react"
 import { useState } from "react"
 
 
@@ -63,8 +63,14 @@ const LoginForm = ({role,title,subtitle}) => {
                </button>
               </div>
           </div>
-          <button>
-            
+          <button type="submit" disabled={loading} 
+          className="w-full py-3 bg-linear-to-r from-indigo-600 to-indigo-500 text-white
+          rounded-md text-sm font-semibold hover:from-indigo-700 hover:to-indigo-600 
+          disabled:opacity-50 transition-all duration-200 shadow-lg shadow-indigo-500/25
+          active:scale-[0.98] flex items-center justify-center">
+            {loading && 
+            <Loader2Icon className="animate-spin h-4 w-4 mr-2"/>}
+            Sign in
           </button>
         </form>
       </div>
